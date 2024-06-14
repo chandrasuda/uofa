c2875c15b2408941353321a0052f56421717e789
chandrasuda <kiran1234c@gmail.com>
2024-06-14 07:10:03 +0530

    init india

--- segmentation.py
+++ segmentation.py
@@ -1,9 +1,8 @@
 import numpy as np
 
-#Use segmentation
-def segment_cough(x,fs, cough_padding=0.2,min_cough_len=0.2, th_l_multiplier = 0.1, th_h_multiplier = 2):
+def segment_cough(x, fs, cough_padding, min_cough_len, th_l_multiplier=0.1, th_h_multiplier=2):
     """Preprocess the data by segmenting each file into individual coughs using a hysteresis comparator on the signal power
-    
+
     Inputs:
     *x (np.array): cough signal
     *fs (float): sampling frequency in Hz
@@ -11,13 +10,12 @@ def segment_cough(x,fs, cough_padding=0.2,min_cough_len=0.2, th_l_multiplier = 0
     *min_cough_length (float): length of the minimum possible segment that can be considered a cough
     *th_l_multiplier (float): multiplier of the RMS energy used as a low threshold of the hysteresis comparator
     *th_h_multiplier (float): multiplier of the RMS energy used as a high threshold of the hysteresis comparator
-    
+
     Outputs:
     *coughSegments (np.array of np.arrays): a list of cough signal arrays corresponding to each cough
     cough_mask (np.array): an array of booleans that are True at the indices where a cough is in progress"""
-                
-    cough_mask = np.array([False]*len(x))
-    
+
+    cough_mask = np.array([False] * len(x))
 
     # define hysteresis thresholds
     rms = np.sqrt(np.mean(np.square(x)))
@@ -26,46 +24,45 @@ def segment_cough(x,fs, cough_padding=0.2,min_cough_len=0.2, th_l_multiplier = 0
 
     # segment coughs
     coughSegments = []
-    padding = round(fs*cough_padding)
-    min_cough_samples = round(fs*min_cough_len)
+    padding = round(fs * cough_padding)
+    min_cough_samples = round(fs * min_cough_len)
+    
+    # If padding and min_cough_len are both 0, return entire segments
+    if cough_padding == 0.0 and min_cough_len == 0.0:
+        coughSegments.append(x)
+        cough_mask[:] = True
+        return coughSegments, cough_mask
+    
     cough_start = 0
     cough_end = 0
     cough_in_progress = False
-    tolerance = round(0.01*fs)
+    tolerance = round(0.01 * fs)
     below_th_counter = 0
-    
-    for i, sample in enumerate(x**2):
+
+    for i, sample in enumerate(x ** 2):
         if cough_in_progress:
             # counting and adding cough samples
-            if sample<seg_th_l:
+            if sample < seg_th_l:
                 below_th_counter += 1
                 if below_th_counter > tolerance:
-                    cough_end = i+padding if (i+padding < len(x)) else len(x)-1
+                    cough_end = i + padding if (i + padding < len(x)) else len(x) - 1
                     cough_in_progress = False
-                    if (cough_end+1-cough_start-2*padding>min_cough_samples):
-                        coughSegments.append(x[cough_start:cough_end+1])
-                        cough_mask[cough_start:cough_end+1] = True
+                    if (cough_end + 1 - cough_start - 2 * padding > min_cough_samples):
+                        coughSegments.append(x[cough_start:cough_end + 1])
+                        cough_mask[cough_start:cough_end + 1] = True
             # cough end
-            elif i == (len(x)-1):
-                cough_end=i
+            elif i == (len(x) - 1):
+                cough_end = i
                 cough_in_progress = False
-                if (cough_end+1-cough_start-2*padding>min_cough_samples):
-                    coughSegments.append(x[cough_start:cough_end+1])
-            # reset counter for number of sample tolerance 
+                if (cough_end + 1 - cough_start - 2 * padding > min_cough_samples):
+                    coughSegments.append(x[cough_start:cough_end + 1])
+            # reset counter for number of sample tolerance
             else:
                 below_th_counter = 0
         else:
             # start cough
-            if sample>seg_th_h:
-                cough_start = i-padding if (i-padding >=0) else 0
+            if sample > seg_th_h:
+                cough_start = i - padding if (i - padding >= 0) else 0
                 cough_in_progress = True
-    
-    return coughSegments, cough_mask
 
-def compute_SNR(x, fs):
-    """Compute the Signal-to-Noise ratio of the audio signal x (np.array) with sampling frequency fs (float)"""
-    segments, cough_mask = segment_cough(x,fs)
-    RMS_signal = 0 if len(x[cough_mask])==0 else np.sqrt(np.mean(np.square(x[cough_mask])))
-    RMS_noise = np.sqrt(np.mean(np.square(x[~cough_mask])))
-    SNR = 0 if (RMS_signal==0 or np.isnan(RMS_noise)) else 20*np.log10(RMS_signal/RMS_noise)
-    return SNR
+    return coughSegments, cough_mask
